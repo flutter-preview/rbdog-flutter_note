5b851125e5bffb74bbe434530d665eddcd1c0098
Rubydog <76613417+rbdog@users.noreply.github.com>
2022-11-04 15:47:59 +0900

    part13 続き

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sample/side_menu.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_sample/page_a.dart';
+import 'package:flutter_sample/page_b.dart';
+import 'package:flutter_sample/page_c.dart';
 
 main() {
-  // アップバー
-  final appBar = AppBar(
-    title: const Text('appBar'),
-  );
-
-  // ドロワー
-  const drawer = Drawer(
-    child: SideMenu(),
-  );
-
-  // エンドドロワー
-  const endDrawer = Drawer(
-    child: SideMenu(),
-  );
-
-  // 画面
-  final scaffold = Scaffold(
-    appBar: appBar,
-    drawer: drawer,
-    endDrawer: endDrawer,
-  );
-
   // アプリ
-  final app = MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: scaffold,
-  );
+  const app = MaterialApp(home: Root());
 
-  runApp(app);
+  // プロバイダースコープでアプリを囲む
+  const scope = ProviderScope(child: app);
+  runApp(scope);
+}
+
+// プロバイダー
+final indexProvider = StateProvider((ref) {
+  // 変化させたいデータ => 0, 1, 2, 3 ...
+  return 0;
+});
+
+class Root extends ConsumerWidget {
+  const Root({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // インデックス
+    final index = ref.watch(indexProvider);
+
+    // アイテムたち
+    const items = [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.person),
+        label: 'アイテムA',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'アイテムB',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.settings),
+        label: 'アイテムC',
+      ),
+    ];
+
+    final bar = BottomNavigationBar(
+      items: items,
+      backgroundColor: Colors.red,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.black,
+      currentIndex: index,
+      onTap: (index) {
+        // タップされたとき インデックスを変更する
+        ref.read(indexProvider.notifier).state = index;
+      },
+    );
+
+    // 画面たち
+    final pages = [
+      PageA(),
+      PageB(),
+      PageC(),
+    ];
+
+    return Scaffold(
+      body: pages[index],
+      bottomNavigationBar: bar,
+    );
+  }
 }
