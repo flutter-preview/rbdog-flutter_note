abea1420be3c0497b6fc682699a45029fa548efa
Rubydog <76613417+rbdog@users.noreply.github.com>
2022-12-18 19:37:17 +0900

    add yuix sample

--- lib/yui/yui.dart
+++ lib/yui/yui.dart
@@ -59,3 +59,28 @@ extension AsYuiWidget on Widget {
     );
   }
 }
+
+void main(List<String> args) {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final yellow = const ColoredBox(color: Colors.yellow).yui(
+      onAppeaer: () {
+        print('Yellow が表示されました');
+      },
+      onDisappeaer: () {
+        print('Yellow が削除されました');
+      },
+      width: 300,
+      height: 300,
+    );
+    return MaterialApp(
+      home: Scaffold(body: Center(child: yellow)),
+    );
+  }
+}
