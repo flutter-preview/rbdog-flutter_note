01aa8d2d0f0092a171f39ce7826dde0c9def2803
Rubydog <76613417+rbdog@users.noreply.github.com>
2022-12-20 20:35:02 +0900

    liveroom

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,271 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:percent_indicator/percent_indicator.dart';
+import 'package:liveroom/liveroom.dart';
 
-// メイン関数
 void main() {
-  const app = MaterialApp(home: Home());
-  const scope = ProviderScope(child: app);
-  runApp(scope);
+  // アプリ全体のデザイン設定
+  final theme = ThemeData(
+    textTheme: const TextTheme(
+      bodyText1: TextStyle(fontSize: 24.0),
+      bodyText2: TextStyle(fontSize: 24.0),
+      button: TextStyle(fontSize: 24.0),
+      subtitle1: TextStyle(fontSize: 24.0),
+      subtitle2: TextStyle(fontSize: 24.0),
+    ),
+  );
+
+  // アプリ本体
+  final app = MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: const HomePage(),
+    theme: theme,
+  );
+  runApp(app);
 }
 
-// パーセントの状態管理
-final percentProvider = StateProvider((ref) {
-  return 0.00;
-});
+//
+// * ライブルームのインスタンス
+//
+final liveroom = Liveroom();
 
-// ホーム画面
-class Home extends ConsumerWidget {
-  const Home({super.key});
+/// ホーム画面
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // パーセント
-    final percent = ref.watch(percentProvider);
+  Widget build(BuildContext context) {
+    final layout = HomePageLayout(
+      // 作成ボタンをタップ
+      onTapCreate: () async {
+        //
+        // * ルームを作成する = create
+        //
+        await liveroom.create(roomId: '0001');
 
-    // 丸型のインジケーター
-    final circular = CircularPercentIndicator(
-      percent: percent,
-      backgroundColor: Colors.yellow,
-      progressColor: Colors.green,
-      animation: true,
-      animationDuration: 200,
-      animateFromLastPercent: true,
-      radius: 60.0,
-      lineWidth: 20.0,
-      center: Text('${percent * 100}%'),
+        // メッセージ画面に進む
+        pushToMessagePage(context);
+      },
+      // 参加ボタンをタップ
+      onTapJoin: () async {
+        //
+        // * ルームに参加する = join
+        //
+        await liveroom.join(roomId: '0001');
+
+        // メッセージ画面に進む
+        pushToMessagePage(context);
+      },
     );
 
-    // 棒型のインジケーター
-    final linear = LinearPercentIndicator(
-      percent: percent,
-      backgroundColor: Colors.yellow,
-      progressColor: Colors.green,
-      animation: true,
-      animationDuration: 200,
-      animateFromLastPercent: true,
-      alignment: MainAxisAlignment.center,
-      lineHeight: 20,
-      width: 300,
-      center: Text('${percent * 100}%'),
+    return layout;
+  }
+
+  /// メッセージ画面へ進む
+  void pushToMessagePage(BuildContext context) {
+    final route = MaterialPageRoute(
+      builder: (context) => const MessagePage(),
+    );
+    Navigator.of(context).push(route);
+  }
+}
+
+/// メッセージ画面
+class MessagePage extends StatefulWidget {
+  const MessagePage({Key? key}) : super(key: key);
+
+  @override
+  MessagePageState createState() => MessagePageState();
+}
+
+class MessagePageState extends State<MessagePage> {
+  final List<String> messages = [];
+
+  @override
+  Widget build(BuildContext context) {
+    final layout = MessagePageLayout(
+      messages: messages,
+
+      // 退出ボタンをタップ
+      onTapExit: () {
+        //
+        // * ルームを退出する = exit
+        //
+        liveroom.exit();
+
+        // 前の画面に戻る
+        popPage(context);
+      },
+
+      // 送信ボタンをタップ
+      onTapSend: (text) {
+        //
+        // * メッセージを送信する = send
+        //
+        liveroom.send(message: text);
+      },
     );
 
-    // ボタン
-    final button = ElevatedButton(
-      onPressed: () => onPressed(ref),
-      child: const Text('スタート'),
-    );
+    //
+    // * ライブルームView を使ってメッセージを受け取る
+    //
+    return LiveroomView(
+      liveroom: liveroom,
 
-    // 縦に並べるカラム
-    final column = Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        circular,
-        linear,
-        button,
-      ],
-    );
+      //
+      // * 誰かがルームに参加したとき = onJoin
+      //
+      onJoin: (userId) {
+        addMessage('参加しました');
+      },
 
-    // 画面の真ん中にカラムを置く
-    return Scaffold(
-      body: Center(
-        child: column,
-      ),
+      //
+      // * メッセージを受け取ったとき = onReceive
+      //
+      onReceive: (userId, message) {
+        // 画面に表示する
+        addMessage(message);
+      },
+
+      //
+      // * 誰かがルームを退出したとき = onExit
+      //
+      onExit: (userId) {
+        addMessage('退出しました');
+      },
+
+      child: layout,
     );
   }
 
-  // ボタンを押したときの関数
-  void onPressed(WidgetRef ref) async {
-    // 1秒まつ
-    await Future.delayed(const Duration(seconds: 1));
-    // 20%
-    ref.read(percentProvider.notifier).state = 0.20;
-    // 1秒まつ
-    await Future.delayed(const Duration(seconds: 1));
-    // 40%
-    ref.read(percentProvider.notifier).state = 0.40;
-    // 1秒まつ
-    await Future.delayed(const Duration(seconds: 1));
-    // 60%
-    ref.read(percentProvider.notifier).state = 0.60;
-    // 1秒まつ
-    await Future.delayed(const Duration(seconds: 1));
-    // 80%
-    ref.read(percentProvider.notifier).state = 0.80;
-    // 1秒まつ
-    await Future.delayed(const Duration(seconds: 1));
-    // 100%
-    ref.read(percentProvider.notifier).state = 1.00;
+  // メッセージを表示する
+  void addMessage(String message) {
+    setState(() {
+      messages.add(message);
+    });
+  }
+
+  // 前の画面に戻る
+  void popPage(BuildContext context) {
+    Navigator.of(context).pop();
+  }
+}
+
+/// ホーム画面のレイアウト
+class HomePageLayout extends StatelessWidget {
+  const HomePageLayout({
+    required this.onTapCreate,
+    required this.onTapJoin,
+    Key? key,
+  }) : super(key: key);
+
+  final void Function() onTapCreate;
+  final void Function() onTapJoin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: onTapCreate,
+              child: const Text('ルーム\n作成'),
+            ),
+            ElevatedButton(
+              onPressed: onTapJoin,
+              child: const Text('ルーム\n参加'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/// メッセージ画面のレイアウト
+class MessagePageLayout extends StatelessWidget {
+  const MessagePageLayout({
+    required this.messages,
+    required this.onTapExit,
+    required this.onTapSend,
+    Key? key,
+  }) : super(key: key);
+
+  final List<String> messages;
+  final void Function() onTapExit;
+  final void Function(String text) onTapSend;
+
+  Widget itemBuilder(BuildContext context, int index) {
+    return Text(messages[index]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+
+    final exitButton = ElevatedButton(
+      onPressed: onTapExit,
+      child: const Text('ルーム退出'),
+    );
+
+    final inputForm = SizedBox(
+      width: 300,
+      height: 50,
+      child: TextField(
+        decoration: const InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+        ),
+        controller: textController,
+      ),
+    );
+
+    final sendButton = ElevatedButton(
+      onPressed: () => onTapSend(textController.text),
+      child: const Text('送信'),
+    );
+
+    final topBar = Container(
+      width: double.infinity,
+      height: 100,
+      color: Colors.grey[300],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(width: 50, height: 50),
+          exitButton,
+        ],
+      ),
+    );
+
+    final messageListView = Expanded(
+      child: ListView.builder(
+        itemBuilder: itemBuilder,
+        itemCount: messages.length,
+      ),
+    );
+
+    final bottomBar = Container(
+      width: double.infinity,
+      height: 100,
+      color: Colors.grey[300],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          inputForm,
+          sendButton,
+        ],
+      ),
+    );
+
+    return Scaffold(
+      body: Column(
+        children: [
+          topBar,
+          messageListView,
+          bottomBar,
+        ],
+      ),
+    );
   }
 }
