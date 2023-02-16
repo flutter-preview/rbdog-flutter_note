af96837f22afaaa8c610a6e3ea5f14bd248fd4d8
Rubydog <hello.rbdog@gmail.com>
2023-02-16 12:32:17 +0900

    reset

---
+++ lib/part20/main.dart
@@ -0,0 +1,36 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+// void main() {
+//   const app = App();
+//   runApp(app);
+// }
+
+// // アプリ本体
+// class App extends StatelessWidget {
+//   const App({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Home(),
+//     );
+//   }
+// }
+
+// // ホーム画面
+// class Home extends StatelessWidget {
+//   const Home({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Text(
+//           'バナナ',
+//           style: GoogleFonts.hachiMaruPop(
+//             fontSize: 30,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
