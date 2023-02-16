6635c53410ba5a68f9b16176854aeed7d4aa19ef
Rubydog <hello.rbdog@gmail.com>
2023-02-16 17:09:30 +0900

    flut20

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
+
 /// メイン関数
-void main() {}
+void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString(
+      'google_fonts/OFL.txt',
+    );
+    yield LicenseEntryWithLineBreaks(
+      ['google_fonts'],
+      license,
+    );
+  });
+
+  const app = MyApp();
+  runApp(app);
+}
+
+/// アプリ本体
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+/// ホーム画面
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              'バナナ美味しい',
+              style: GoogleFonts.hachiMaruPop(
+                fontSize: 30,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+
+            // ボタン
+            ElevatedButton(
+              onPressed: () {
+                showLicensePage(context: context);
+              },
+              child: Text('ボタン'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/part20/main.dart
+++ lib/part20/main.dart
@@ -1,36 +1,75 @@
-// import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-// void main() {
-//   const app = App();
-//   runApp(app);
-// }
+/// メイン関数
+void main() {
+  // OFL を守るために このコードが必要
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString(
+      'google_fonts/OFL.txt',
+    );
+    yield LicenseEntryWithLineBreaks(
+      ['google_fonts'],
+      license,
+    );
+  });
 
-// // アプリ本体
-// class App extends StatelessWidget {
-//   const App({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       home: Home(),
-//     );
-//   }
-// }
+  // アプリ
+  const app = MyApp();
+  runApp(app);
+}
 
-// // ホーム画面
-// class Home extends StatelessWidget {
-//   const Home({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: Text(
-//           'バナナ',
-//           style: GoogleFonts.hachiMaruPop(
-//             fontSize: 30,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+/// アプリ本体
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+/// ホーム画面
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // テキスト
+    final text = Text(
+      'バナナ美味しい',
+      style: GoogleFonts.hachiMaruPop(
+        fontSize: 30,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+
+    // ボタンを押した時の関数
+    void onPressed() {
+      showLicensePage(context: context);
+    }
+
+    // ボタン
+    final button = ElevatedButton(
+      onPressed: onPressed,
+      child: Text('ボタン'),
+    );
+
+    return Scaffold(
+      body: Center(
+        // カラムで縦に並べる
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            text,
+            button,
+          ],
+        ),
+      ),
+    );
+  }
+}
