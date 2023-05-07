b4bac15783fd1f0bfe0629be2c045c82a64d077b
Rubydog <76613417+rbdog@users.noreply.github.com>
2023-05-07 23:15:40 +0900

    answer QA

---
+++ lib/qa/text_form_riverpod.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'text_form_riverpod.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$textNotifierHash() => r'13d317c5fd238710c716ac65960298cc23d6a99c';
+
+/// See also [TextNotifier].
+@ProviderFor(TextNotifier)
+final textNotifierProvider =
+    AutoDisposeNotifierProvider<TextNotifier, String>.internal(
+  TextNotifier.new,
+  name: r'textNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$textNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TextNotifier = AutoDisposeNotifier<String>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
